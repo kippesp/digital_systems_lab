8658bf6b8f10bf30bda854204a56bbd8458877cc
Paul Kippes <kippesp@gmail.com>
2020-06-28 22:01:23 -0700

    First file

---
+++ lab2/part1/simple_counter/simple_counter.srcs/sources_1/new/seven_seg_controller.sv
@@ -0,0 +1,54 @@
+module seven_seg_controller(input               clk_in,
+                            input               rst_in,
+                            input [31:0]        val_in,
+                            output logic[7:0]   cat_out,
+                            output logic[7:0]   an_out
+    );
+  
+    logic[7:0]      segment_state;
+    logic[31:0]     segment_counter;
+    logic [3:0]     routed_vals;
+    logic [6:0]     led_out;
+    
+    binary_to_seven_seg my_converter ( .val_in(routed_vals), .led_out(led_out));
+    assign cat_out = ~led_out;
+    assign an_out = ~segment_state;
+
+    
+    always_comb begin
+        case(segment_state)
+            8'b0000_0001:   routed_vals = val_in[3:0];
+            8'b0000_0010:   routed_vals = val_in[7:4];
+            8'b0000_0100:   routed_vals = val_in[11:8];
+            8'b0000_1000:   routed_vals = val_in[15:12];
+            8'b0001_0000:   routed_vals = val_in[19:16];
+            8'b0010_0000:   routed_vals = val_in[23:20];
+            8'b0100_0000:   routed_vals = val_in[27:24];
+            8'b1000_0000:   routed_vals = val_in[31:28];
+            default:        routed_vals = val_in[3:0];       
+        endcase
+    end
+    
+    always_ff @(posedge clk_in)begin
+        if (rst_in)begin
+            segment_state <= 8'b0000_0001;
+            segment_counter <= 32'b0;
+        end else begin
+            if (segment_counter == 32'd100_000)begin
+                segment_counter <= 32'd0;
+                segment_state <= {segment_state[6:0],segment_state[7]};
+            end else begin
+                segment_counter <= segment_counter +1;
+            end
+        end
+    end
+        
+endmodule //seven_seg_controller
+
+
+//feel free to either include binary_to_seven_seg module here or in its own file!
+module binary_to_seven_seg ( input [3:0] val_in, output logic [6:0] led_out);
+    //
+    //  your previously written function
+    //
+endmodule // binary_to_seven
