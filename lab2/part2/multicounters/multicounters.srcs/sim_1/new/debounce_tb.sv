6569b53494124e166a38e0df0c487e280ee613a3
Paul Kippes <kippesp@gmail.com>
2020-07-03 23:35:34 -0700

    simulation changes

--- lab2/part2/multicounters/multicounters.srcs/sim_1/new/debounce_tb.sv
+++ lab2/part2/multicounters/multicounters.srcs/sim_1/new/debounce_tb.sv
@@ -1,18 +1,15 @@
 `timescale 1ns / 1ps
-
 module debounce_tb;
-    
     //make logics for inputs and outputs!
     logic clk;
     logic rst;
-    logic in;
-    logic out;
-    
-    debounce my_debouncer(  .clk_in(clk),
-                            .rst_in(rst),
-                            .bouncey_in(in),
-                            .clean_out(out));                            
-    
+    logic dirty;
+    logic clean;
+    //make an instance of the counter...call it uut or my_counter or something else
+    debounce uut(               .clk_in(clk),
+                                .rst_in(rst),
+                                .bouncey_in(dirty),
+                                .clean_out(clean));
     //An always block in simulation **always** runs in the background
     //this is your standard way of making a clock below:
     //it says: every 5 ns, make clk be !clk
@@ -21,30 +18,19 @@ module debounce_tb;
         #5;  //every 5 ns switch...so period of clock is 10 ns...100 MHz clock
         clk = !clk;
     end
-    
     //initial block...this is our test simulation
     initial begin
         $display("Starting Sim"); //print nice message
         clk = 0; //initialize clk (super important)
         rst = 0; //initialize rst (super important)
-        in = 0; //initialize evt (super important)
+        dirty = 0; //initialize evt (super important)
         #20  //wait a little bit of time at beginning
         rst = 1; //reset system
         #20; //hold high for a few clock cycles
-        rst = 0; //pull low
-        #20;
-        in = 1;
-        #10
-        in = 0;
-        #30
-        in = 1;
-        #10
-        in = 0;
-        #30
-        in = 1;
-        #10000000
-        $finish;
-    
-  
+        rst=0; //pull low
+        #20; //wait a little bit
+        dirty = 1; //push the button
+        #15000000; //wait a bit...
+        dirty = 0; //release the button
     end
-endmodule
+endmodule //debounce_tb
